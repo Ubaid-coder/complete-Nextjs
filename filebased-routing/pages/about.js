36c262103d178b5890464c66e59ea0e104c42bc7
Muhammad Ubaid <mu0826960@gmail.com>
2024-09-12 17:45:27 +0500

    .

---
+++ filebased-routing/pages/about.js
@@ -0,0 +1,11 @@
+
+
+function about() {
+  return (
+    <>
+    <h1>Hello welcome to the about page</h1>
+    </>
+  )
+}
+
+export default about
---
+++ filebased-routing/pages/blogged/how.js
@@ -0,0 +1,13 @@
+
+
+function how() {
+    return (
+        <>
+        <h1> how to use keyboard</h1>
+        
+
+        </>
+    )
+}
+
+export default how;
