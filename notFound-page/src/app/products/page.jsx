ff47c30c9c14baebd72e46908f918472b3bcc90a
Muhammad Ubaid <mu0826960@gmail.com>
2024-09-13 09:17:54 +0500

    .

---
+++ notFound-page/src/app/products/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function products() {
+  return (
+    <>
+    <h1>Product Lists</h1>
+    <h2>Product1</h2>
+    <h2>Product2</h2>
+    <h2>Product3</h2>
+    </>
+  )
+}
