35ee9ed57f096ca96b9e282b29d31812bc8d5a22
Muhammad Ubaid <mu0826960@gmail.com>
2024-10-22 23:11:52 +0500

    .

---
+++ csr-vs-ssr/app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>HelloWorld</h1>
+      <Link href="/users">Users</Link>
+      <ProductCard/>
+    </main>
+  );
+}
