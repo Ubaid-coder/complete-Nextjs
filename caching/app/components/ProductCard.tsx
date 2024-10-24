88270e92a2af1c5745a364e190b3d9b72ef25f91
Muhammad Ubaid <mu0826960@gmail.com>
2024-10-24 19:30:30 +0500

    .

---
+++ caching/app/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+import AddToCart from "./AddToCart";
+
+
+function ProductCard() {
+
+  return (
+    <div className="py-5">
+      <h1>This is our products</h1>
+      <p>Add your product cards here</p>
+      <AddToCart/>
+    </div>
+  );
+}
+
+export default ProductCard;
